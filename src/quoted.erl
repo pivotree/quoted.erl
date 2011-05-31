f9c542fab3a9aad3c2eeb17035046018389f0450
Magnus Klaar <magnus.klaar@sgsstudentbostader.se>
2011-05-31 12:07:56 +0200

    remove NIF implementation and use a minimal .erl one

--- src/quoted.erl
+++ src/quoted.erl
@@ -1,260 +1,158 @@
 -module(quoted).
+-compile(inline).
+-export([to_url/1,
+         from_url/1]).
 
--export([to_list/1,
-         to_binary/1,
-         as_list/1,
-         as_binary/1]).
+-type data()    :: [byte()] | binary().
 
-%% internal exports
--on_load(load_nif/0).
--export([load_nif/0,
-         '_nif_loaded'/0,
-         '_nif_unquote'/2,
-         '_erl_unquote'/2,
-         '_nif_quote'/2,
-         '_erl_quote'/2,
-         test/0]).
-
-load_nif() ->
-    catch erlang:load_nif(nif_path(), 0),
-    ok.
-
-nif_path() ->
-    So = "quoted",
-    case code:priv_dir(quoted) of
-        {error, bad_name} ->
-            case code:which(?MODULE) of
-                File when not is_list(File) -> filename:join("../priv", So);
-                File -> filename:join([filename:dirname(File),"../priv", So])
-            end;
-         Dir ->
-            filename:join(Dir, So)
-    end.
-
--spec '_nif_loaded'() -> boolean().
-'_nif_loaded'() ->
-    false.
-
-%% Fall back to erlang implementation if the nif can't be loaded
--spec '_nif_unquote'(string() | binary(), list | binary) -> string() | binary().
-'_nif_unquote'(String, Format) ->
-    '_erl_unquote'(String, Format).
+-spec to_url(data()) -> data().
+to_url(Str) when is_list(Str) ->
+    quote_list_to_list(Str);
+to_url(Bin) when is_binary(Bin) ->
+    quote_bin_to_bin(Bin).
 
 
--spec '_erl_unquote'(string() | binary(), list | binary) -> string() | binary().
-'_erl_unquote'(String, binary) when is_binary(String) ->
-    unquote_bin(String);
-
-'_erl_unquote'(String, binary) when is_list(String) ->
-    list_to_binary(unquote_list(String));
-
-'_erl_unquote'(String, list) when is_list(String) ->
-    unquote_list(String);
-
-'_erl_unquote'(String, list) when is_binary(String) ->
-    binary_to_list(unquote_bin(String)).
+-spec from_url(data()) -> data().
+from_url(Str) when is_list(Str) ->
+    unquote_list_to_list(Str);
+from_url(Bin) when is_binary(Bin) ->
+    unquote_bin_to_bin(Bin).
 
 
-%% Fall back to erlang implementation if the nif can't be loaded
--spec '_nif_quote'(string() | binary(), list | binary) -> string() | binary().
-'_nif_quote'(String, Format) ->
-    '_erl_quote'(String, Format).
+-spec unquote_list_to_list([byte()]) -> [byte()].
+unquote_list_to_list([$%,HH,HL|T]) when is_integer(HH), is_integer(HL) ->
+    H = unhex(HH),
+    L = unhex(HL),
+    C = tobyte(H, L),
+    [C|unquote_list_to_list(T)];
+unquote_list_to_list([H|T]) when is_integer(H) ->
+    [H|unquote_list_to_list(T)];
+unquote_list_to_list([]) ->
+    [].
 
 
--spec '_erl_quote'(string() | binary(), list | binary) -> string() | binary().
-'_erl_quote'(String, binary) when is_binary(String) ->
-    quote_bin(String);
+-spec unquote_bin_to_bin(binary()) -> binary().
+unquote_bin_to_bin(Bin) when is_binary(Bin) ->
+    unquote_bin_to_bin(Bin, <<>>).
 
-'_erl_quote'(String, binary) when is_list(String) ->
-    list_to_binary(quote_list(String));
-
-'_erl_quote'(String, list) when is_list(String) ->
-    quote_list(String);
-
-'_erl_quote'(String, list) when is_binary(String) ->
-    binary_to_list(quote_bin(String)).
-
-
--spec to_list(string() | binary()) -> string().
-to_list(String) ->
-    '_nif_unquote'(String, list).
+-spec unquote_bin_to_bin(binary(), binary()) -> binary().
+unquote_bin_to_bin(<<$%,HH,HL,T/binary>>, Acc) ->
+    H = unhex(HH),
+    L = unhex(HL),
+    C = tobyte(H, L),
+    unquote_bin_to_bin(T, <<Acc/binary, C>>);
+unquote_bin_to_bin(<<C, T/binary>>, Acc) ->
+    unquote_bin_to_bin(T, <<Acc/binary, C>>);
+unquote_bin_to_bin(<<>>, Acc) ->
+    Acc.
 
 
--spec to_binary(string() | binary()) -> binary().
-to_binary(String) ->
-    '_nif_unquote'(String, binary).
-
-
--spec as_list(string() | binary()) -> string().
-as_list(String) ->
-    '_nif_quote'(String, list).
-
-
--spec as_binary(string() | binary()) -> binary().
-as_binary(String) ->
-    '_nif_quote'(String, binary).
-
-
-test_decode(N, Name, Input) ->
-    Bin = iolist_to_binary(Input),
-    Str = binary_to_list(Bin),
-    [Name,
-    {test_type, nif_impl, erl_impl},
-    {list_to_list,
-        timeit(N, fun() -> quoted:to_list(Str) end),
-        timeit(N, fun() -> quoted:'_erl_unquote'(Str, list) end)},
-    {list_to_bin,
-        timeit(N, fun() -> quoted:to_binary(Str) end),
-        timeit(N, fun() -> quoted:'_erl_unquote'(Str, binary) end)},
-    {bin_to_bin,
-        timeit(N, fun() -> quoted:to_binary(Bin) end),
-        timeit(N, fun() -> quoted:'_erl_unquote'(Bin, binary) end)},
-    {bin_to_list,
-        timeit(N, fun() -> quoted:to_list(Bin) end),
-        timeit(N, fun() -> quoted:'_erl_unquote'(Bin, binary) end)}
-    ].
-
-test_encode(N, Name, Input) ->
-    Bin = iolist_to_binary(Input),
-    Str = binary_to_list(Bin),
-    [Name,
-    {test_type, nif_impl, erl_impl},
-    {list_to_list,
-        timeit(N, fun() -> quoted:as_list(Str) end),
-        timeit(N, fun() -> quoted:'_erl_quote'(Str, list) end)},
-    {list_to_bin,
-        timeit(N, fun() -> quoted:as_binary(Str) end),
-        timeit(N, fun() -> quoted:'_erl_quote'(Str, binary) end)},
-    {bin_to_bin,
-        timeit(N, fun() -> quoted:as_binary(Bin) end),
-        timeit(N, fun() -> quoted:'_erl_quote'(Bin, binary) end)},
-    {bin_to_list,
-        timeit(N, fun() -> quoted:as_list(Bin) end),
-        timeit(N, fun() -> quoted:'_erl_quote'(Bin, binary) end)}
-    ].
-
-
-
-
-
-
-test_32b(N) ->
-    test_decode(N, 'Decode 32B unquoted string', ["a" || _ <- lists:seq(1, 32)]) ++
-    test_encode(N, 'Encode 32B unquoted string', ["a" || _ <- lists:seq(1, 32)]).
-
-test_1kb(N) ->
-    test_decode(N, 'Decode 1KB unquoted string', ["a" || _ <- lists:seq(1, 1024)]) ++
-    test_encode(N, 'Encode 1KB unquoted string', ["a" || _ <- lists:seq(1, 1024)]).
-
-test_2kb(N) ->
-    test_decode(N, 'Decode 2KB quoted string', ["%20" || _ <- lists:seq(1, 2048 div 3)]) ++
-    test_encode(N, 'Encode 2KB quoted string', [" "   || _ <- lists:seq(1, 2048)]).
-
-test() ->
-    R = test_32b(10000)
-    ++  test_1kb(10000)
-    ++  test_2kb(10000),
-    lists:foreach(fun(Res) ->
-        io:format("~w~n", [Res])
-    end, R).
-
-
-timeit(Times, Fun) ->
-    Tmp = lists:seq(1, Times),
-    {Time, ok} = timer:tc(fun() ->
-        [Fun() || _ <- Tmp],
-        ok
-    end, []),
-    Time / Times.
-
-
-%% Function for unquoting query string parameters borrowed from mochiweb.
-%% slightly modified to not use an accumulator.
-
-
--spec unquote_list(string()) -> string().
-unquote_list(String) when is_list(String) ->
-    unquote_list_(String).
-
-
-unquote_list_([]) ->
-    [];
-
-unquote_list_("%"   ++ [H,L|T]) ->
-    is_hex(H) andalso is_hex(L) orelse erlang:error(badarg),
-    Char = unhex(L) bor (unhex(H) bsl 4),
-    [Char|unquote_list_(T)];
-
-unquote_list_([Char|T]) ->
-    [Char|unquote_list_(T)].
-
--spec unquote_bin(binary()) -> binary().
-unquote_bin(String) when is_binary(String) ->
-    unquote_bin_(String, <<>>).
-
-
-unquote_bin_(<<>>, Acc) ->
-    Acc;
-
-unquote_bin_(<<"%", H, L, Rest/binary>>, Acc) ->
-    is_hex(H) andalso is_hex(L) orelse erlang:error(badarg),
-    Char = unhex(L) bor (unhex(H) bsl 4),
-    unquote_bin_(Rest, <<Acc/binary, Char>>);
-
-unquote_bin_(<<Char, Rest/binary>>, Acc) ->
-    unquote_bin_(Rest, <<Acc/binary, Char>>).
-
-
--spec quote_list(string()) -> string().
-quote_list([]) ->
-    [];
-
-quote_list([Char|T]) ->
-    case is_safe(Char) of
+-spec quote_list_to_list([byte()]) -> [byte()].
+quote_list_to_list([C|T]) ->
+    case is_url_safe(C) of
         true ->
-            [Char|quote_list(T)];
+            [C|quote_list_to_list(T)];
         false ->
-            HH = tohex(Char bsr 4),
-            HL = tohex(Char band 15),
-            [$%, HH, HL|quote_list(T)]
-    end.
+            H = tohex(highbits(C)),
+            L = tohex(lowbits(C)),
+            [$%,H,L|quote_list_to_list(T)]
+    end;
+quote_list_to_list([]) ->
+    [].
 
 
--spec quote_bin(binary()) -> binary().
-quote_bin(String) ->
-    quote_bin(String, <<>>).
+-spec quote_bin_to_bin(binary()) -> binary().
+quote_bin_to_bin(Bin) when is_binary(Bin) ->
+    quote_bin_to_bin(Bin, <<>>).
 
-quote_bin(<<>>, Acc) ->
-    Acc;
-quote_bin(<<Char, Rest/binary>>, Acc) ->
-    case is_safe(Char) of
+-spec quote_bin_to_bin(binary(), binary()) -> binary().
+quote_bin_to_bin(<<C, T/binary>>, Acc) ->
+    case is_url_safe(C) of
         true ->
-            quote_bin(Rest, <<Acc/binary, Char>>);
+            quote_bin_to_bin(T, <<Acc/binary, C>>);
         false ->
-            HH = tohex(Char bsr 4),
-            LH = tohex(Char band 15),
-            quote_bin(Rest, <<Acc/binary, $%, HH, LH>>)
+            H = tohex(highbits(C)),
+            L = tohex(lowbits(C)),
+            quote_bin_to_bin(T, <<Acc/binary, $%, H, L>>)
+    end;
+quote_bin_to_bin(<<>>, Acc) ->
+    Acc.
+
+
+-spec highbits(byte()) -> byte().
+highbits(I) -> I bsr 4.
+
+-spec lowbits(byte()) -> byte().
+lowbits(I)  -> I band 16#0F.
+
+-spec tobyte(byte(), byte()) -> byte().
+tobyte(H, L) -> L bor (H bsl 4).
+
+-spec tohex(byte()) -> byte().
+tohex(C) ->
+    case C of
+        0  -> $0;
+        1  -> $1;
+        2  -> $2;
+        3  -> $3;
+        4  -> $4;
+        5  -> $5;
+        6  -> $6;
+        7  -> $7;
+        8  -> $8;
+        9  -> $9;
+        10 -> $A;
+        11 -> $B;
+        12 -> $C;
+        13 -> $A;
+        14 -> $E;
+        15 -> $F
+    end.
+
+-spec unhex(byte()) -> byte().
+unhex(C) ->
+    case C of
+        $0 -> 0;
+        $1 -> 1;
+        $2 -> 2;
+        $3 -> 3;
+        $4 -> 4;
+        $5 -> 5;
+        $6 -> 6;
+        $7 -> 7;
+        $8 -> 8;
+        $9 -> 9;
+        $A -> 10; $a -> 10;
+        $B -> 11; $b -> 11;
+        $C -> 12; $c -> 12;
+        $D -> 13; $d -> 13;
+        $E -> 14; $e -> 14;
+        $F -> 15; $f -> 15
     end.
 
 
-unhex(C) when C >= $a, C =< $f -> C - $a + 10;
-unhex(C) when C >= $A, C =< $F -> C - $A + 10;
-unhex(C) when C >= $0, C =< $9 -> C - $0.
-
-tohex(C) when C < 10 -> $0 + C;
-tohex(C) when C < 16 -> $A + (C - 10).
-
-
-is_hex(C) when C >= $0, C =< $9 -> true;
-is_hex(C) when C >= $a, C =< $f -> true;
-is_hex(C) when C >= $A, C =< $F -> true;
-is_hex(_) -> false.
-
-is_safe($.) -> true;
-is_safe($-) -> true;
-is_safe($~) -> true;
-is_safe($_) -> true;
-is_safe(C) when C >= $a, C =< $z -> true;
-is_safe(C) when C >= $A, C =< $Z -> true;
-is_safe(C) when C >= $0, C =< $9 -> true;
-is_safe(_) -> false.
+-spec is_url_safe(byte()) -> boolean().
+is_url_safe(C) ->
+    case C of
+        %% Lowercase
+        $a -> true; $b -> true; $c -> true; $d -> true; $e -> true;
+        $f -> true; $g -> true; $h -> true; $i -> true; $j -> true;
+        $k -> true; $l -> true; $m -> true; $n -> true; $o -> true;
+        $p -> true; $q -> true; $r -> true; $s -> true; $t -> true;
+        $u -> true; $v -> true; $w -> true; $x -> true; $y -> true;
+        $z -> true;
+        %% Uppercase
+        $A -> true; $B -> true; $C -> true; $D -> true; $E -> true;
+        $F -> true; $G -> true; $H -> true; $I -> true; $J -> true;
+        $K -> true; $L -> true; $M -> true; $N -> true; $O -> true;
+        $P -> true; $Q -> true; $R -> true; $S -> true; $T -> true;
+        $U -> true; $V -> true; $W -> true; $X -> true; $Y -> true;
+        $Z -> true;
+        %% Numbers
+        $0 -> true; $1 -> true; $2 -> true; $3 -> true; $4 -> true;
+        $5 -> true; $6 -> true; $7 -> true; $8 -> true; $9 -> true;
+        %% Exceptions
+        $. -> true; $- -> true; $~ -> true; $_ -> true;
+        %% Unsafe
+        _ -> false
+    end.
